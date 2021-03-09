b8f0eda4b5d77f75a094c3255db5adf04d03db8f
mainieri@tm <mainieri.tm@gmail.com>
2021-03-09 10:08:09 +0100

    Primo Commit Client e Server
    Primo Commit Client e Server

---
+++ rubrica-gruppo3-server/src/main/java/it/iad/rubricagruppo3/RubricaGruppo3Application.java
@@ -0,0 +1,13 @@
+package it.iad.rubricagruppo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaGruppo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaGruppo3Application.class, args);
+	}
+
+}
---
+++ rubrica-gruppo3-server/src/test/java/it/iad/rubricagruppo3/RubricaGruppo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.rubricagruppo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricaGruppo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
